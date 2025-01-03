eeae176940c60b2e1ac06108528496e581b6a227
XotEmBotZ <xotembotzop@gmail.com>
2025-01-03 17:40:26 +0530

    Used new HMA style

--- pandas_ta/overlap/ehma.py
+++ pandas_ta/overlap/ehma.py
@@ -1,70 +1,62 @@
 # -*- coding: utf-8 -*-
-from numpy import sqrt as npSqrt
+from numpy import sqrt
+from pandas import Series
+from pandas_ta._typing import DictLike, Int
+from pandas_ta.utils import v_offset, v_pos_default, v_series
 from .ema import ema
-from pandas_ta.utils import get_offset, verify_series
 
 
-def ehma(close, length=None, offset=None, **kwargs):
-    """Indicator: Exponnetial Hull Moving Average (HMA)"""
-    # Validate Arguments
-    length = int(length) if length and length > 0 else 10
-    close = verify_series(close, length)
-    offset = get_offset(offset)
 
-    if close is None: return
+def ehma(
+    close: Series, length: Int = None,
+    offset: Int = None, **kwargs: DictLike
+) -> Series:
+    """Hull Moving Average (EHMA)
 
-    # Calculate Result
+    The Hull Exponential Moving Average attempts to reduce or remove lag
+    in moving averages.
+
+    Sources:
+        https://alanhull.com/hull-moving-average
+
+    Args:
+        close (pd.Series): Series of 'close's
+        length (int): It's period. Default: 10
+        offset (int): How many periods to offset the result. Default: 0
+
+    Kwargs:
+        fillna (value, optional): pd.DataFrame.fillna(value)
+
+    Returns:
+        pd.Series: New feature generated.
+    """
+    # Validate
+    length = v_pos_default(length, 10)
+    close = v_series(close, length + 2)
+
+    if close is None:
+        return
+
+    offset = v_offset(offset)
+
+    # Calculate
     half_length = int(length / 2)
-    sqrt_length = int(npSqrt(length))
+    sqrt_length = int(sqrt(length))
 
     emaf = ema(close=close, length=half_length)
     emas = ema(close=close, length=length)
-    ehma = ema(close=(2 * emaf - emas), length=sqrt_length)
+    ehma = ema(close=2 * emaf - emas, length=sqrt_length)
 
     # Offset
     if offset != 0:
         ehma = ehma.shift(offset)
 
-    # Handle fills
+    # Fill
     if "fillna" in kwargs:
         ehma.fillna(kwargs["fillna"], inplace=True)
-    if "fill_method" in kwargs:
-        ehma.fillna(method=kwargs["fill_method"], inplace=True)
 
-    # Name & Category
+    # Name and Category
     ehma.name = f"EHMA_{length}"
     ehma.category = "overlap"
 
     return ehma
-
-
-ehma.__doc__ = \
-"""Exponential Hull Moving Average (HMA)
-
-The Hull Exponential Moving Average attempts to reduce or remove lag in moving
-averages.
-
-
-Calculation:
-    Default Inputs:
-        length=10
-    EMA = Weighted Moving Average
-    half_length = int(0.5 * length)
-    sqrt_length = int(sqrt(length))
-
-    emaf = EMA(close, half_length)
-    emas = EMA(close, length)
-    EHMA = EMA(2 * emaf - emas, sqrt_length)
-
-Args:
-    close (pd.Series): Series of 'close's
-    length (int): It's period. Default: 10
-    offset (int): How many periods to offset the result. Default: 0
-
-Kwargs:
-    fillna (value, optional): pd.DataFrame.fillna(value)
-    fill_method (value, optional): Type of fill method
-
-Returns:
-    pd.Series: New feature generated.
-"""
